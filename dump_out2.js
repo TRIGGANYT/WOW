a2ebc1c99a3c0a2d42501dcac17e87c8e696d458
Florin.Gerig <florin.gerig@icloud.com>
2026-06-17 16:02:10 +0200

    chore: remove debug scripts and temp files, update gitignore

--- dump_out2.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const buf = fs.readFileSync('out2.bin');
-const text = buf.toString('utf8');
-console.log(text.replace(/\r/g, '\\r').replace(/\n/g, '\\n\n'));
--- dump_status.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const buf = fs.readFileSync('out_status.bin');
-let text = buf.toString('utf8');
-console.log(text.replace(/\r/g, '\\r').replace(/\n/g, '\\n\n'));
